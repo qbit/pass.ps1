f04364a71c1362505430e261124746c67cf5527b
Aaron Bieber <aaron@bolddaemon.com>
2017-09-15 19:19:34 -0600

    formatting

--- pass.ps1
+++ pass.ps1
@@ -7,29 +7,31 @@ function List () {
 }
 
 function Decrypt ($name) {
-    $fn="c:\{0}\.password-store\{1}.gpg" -f $env:HOMEPATH, ($name).Replace('/','\')
+    $fn = "c:\{0}\.password-store\{1}.gpg" -f $env:HOMEPATH, ($name).Replace('/', '\')
 
     if (Test-Path -Path $fn) {
         $out = (& gpg2 -d $fn)
         $out | Select-Object -First 1 | Set-Clipboard
         Write-Host ("{0} coppied to clipboard! Clearing in {1} seconds" -f $name, 45)
 
-	$null = $fork = [System.Management.Automation.PowerShell]::Create()
-	$null = $fork.AddScript(( {
-		param ($clearTime)
-		Start-Sleep -Seconds $clearTime
-		$null|clip
-	}))
+        $null = $fork = [System.Management.Automation.PowerShell]::Create()
+        $null = $fork.AddScript(( {
+                    param ($clearTime)
+                    Start-Sleep -Seconds $clearTime
+                    $null|clip
+                }))
 
-	$null = $fork.AddParameter('clearTime', $clearTime)
-	$null = $fork.BeginInvoke()
-    } else {
+        $null = $fork.AddParameter('clearTime', $clearTime)
+        $null = $fork.BeginInvoke()
+    }
+    else {
         Write-Host ("{0} doesn't exist!" -f $fn)
     }
 }
 
 if ($args.Length -gt 0) {
     Decrypt($args[0])
-} else {
+}
+else {
     List
 }
